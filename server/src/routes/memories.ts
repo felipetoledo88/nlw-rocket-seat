5713a6d88ab4aa04105a89ed89873cae75df6dee
FelipeToledo <felipetoledo.nextage@gmail.com>
2023-05-17 21:51:41 -0300

    feat: finalizado crud memories

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { prisma } from '../lib/prisma'
+import { request } from 'http'
 
 export async function memoriesRoutes(app: FastifyInstance) {
   app.get('/memories', async () => {
@@ -53,9 +54,41 @@ export async function memoriesRoutes(app: FastifyInstance) {
     return memory
   })
 
-  app.put('/memories/:id', async () => {
+  app.put('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = paramsSchema.parse(request.params)
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+   const memory = await prisma.memory.update({
+      where: {
+        id,
+      },
+      data:{
+        content,
+        coverUrl,
+        isPublic
+      }
+    })
+    return memory
   })
 
-  app.delete('/memories/:id', async () => {
+  app.delete('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = paramsSchema.parse(request.params)
+
+    await prisma.memory.delete({
+      where: {
+        id,
+      },
+    })
   })
 }
