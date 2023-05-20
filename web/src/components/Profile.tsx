02ab441eff0c0ce6e8114cbfef02f3e28de1ea34
FelipeToledo <felipetoledo.nextage@gmail.com>
2023-05-20 16:17:46 -0300

    middleware frontend finalizado

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -1,6 +1,5 @@
 import { getUser } from "@/lib/auth";
 import Image from "next/image";
-import Link from "next/link";
 
 export function Profile(){
   const { name, avatarUrl } = getUser()
@@ -9,11 +8,11 @@ export function Profile(){
     <Image src={avatarUrl} width={40} height={40} alt="" className="w-10 h-10 rounded-full" />
    <p className="max-w-[140px] text-sm leading-snug">
     {name}
-    <Link 
+    <a 
     href="/api/auth/logout" 
     className="block text-red-400 hover:text-red-300">
       Sair
-    </Link>     
+    </a>     
    </p>
  </div>
   )
