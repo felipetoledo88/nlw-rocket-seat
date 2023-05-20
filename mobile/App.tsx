82402d255dbc4e36ed4d640205808cc85695dba5
FelipeToledo <felipetoledo.nextage@gmail.com>
2023-05-20 10:37:20 -0300

    feat: pegando token github mobile e salvando no securityStore

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import * as SecureStore from 'expo-secure-store'
 
 import {
   useFonts,
@@ -13,8 +14,17 @@ import blurBg from './src/assets/bg-blur.png'
 import Stripes from './src/assets/stripes.svg'
 import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
 import { styled } from 'nativewind'
+import { useAuthRequest, makeRedirectUri } from 'expo-auth-session'
+import { useEffect } from 'react'
+import { api } from './src/lib/api'
 
 const StyledStripes = styled(Stripes)
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: 'https://github.com/settings/connections/applications/6dd9603a29aa058a62e3',
+};
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -23,6 +33,31 @@ export default function App() {
     BaiJamjuree_700Bold,
   })
 
+  const [response, signInWithGitHub] = useAuthRequest(
+    {
+      clientId: '6dd9603a29aa058a62e3',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery
+  );  
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+      api.post('/register', {
+        code,
+      }).then(response => {
+        const { token } = response.data
+
+        SecureStore.setItemAsync('token', token)
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  }, [response]);
+ 
   if (!hasLoadedFonts) {
     return null
   }
@@ -51,6 +86,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGitHub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembrança
