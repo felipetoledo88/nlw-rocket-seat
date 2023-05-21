53d04feee1a03ba40455684dfdc9ef3f14414576
FelipeToledo <felipetoledo.nextage@gmail.com>
2023-05-21 08:18:26 -0300

    upload backend finalizado

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,23 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
-import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
-import 'dotenv/config'
+import  multipart  from '@fastify/multipart'
+import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
-
-// HTTP Method: GET, POST, PUT, PATCH, DELETE
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+app.register(uploadRoutes)
 app.register(cors, {
   origin: true,
 })
