252c5b2043de3d4e664957c266b058cd748b95a3
FelipeToledo <felipetoledo.nextage@gmail.com>
2023-05-21 08:49:00 -0300

    feat: preview imagem

--- web/src/app/memories/new/page.tsx
+++ web/src/app/memories/new/page.tsx
@@ -1,3 +1,4 @@
+import { MediaPicker } from "@/components/MediaPicker";
 import { Camera, ChevronLeft } from "lucide-react";
 import Link from "next/link";
 
@@ -20,18 +21,17 @@ export default function NewMemories(){
           </label>
 
           <label 
-          htmlFor="isPublic" 
-          className="flex items-center gap-1.5 text-sm  text-gray-200 hover:text-gray-100">
+             htmlFor="isPublic" 
+             className="flex items-center gap-1.5 text-sm  text-gray-200 hover:text-gray-100">
             <input type="checkbox" name="isPublic" id="isPublic" value="true" className="h-4 w-4 rounded border-gray-400 bg-gray-400 text-purple-500"/>
             Tornar memória pública
           </label>
         </div>
-        <input type="file" id="media" className="invisible h-0 w-0"></input>
+        <MediaPicker/>
         <textarea 
-        name="content"
-        spellCheck={false}
-        className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100
-         place-holder:text-gray-400 focus:ring-0"
+         name="content"
+         spellCheck={false}
+         className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 place-holder:text-gray-400 focus:ring-0"
          placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
         />
       </form>
